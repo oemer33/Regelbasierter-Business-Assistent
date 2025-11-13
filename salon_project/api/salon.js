8f6ba917c864e9d592c3fdd46ca66624c1eb8b5e
oemer33 <otosun750@icloud.com>
2025-11-13 13:27:35 +0100

    Delete salon_project directory

--- salon_project/api/salon.js
+++
@@ -1,2 +0,0 @@
-const { loadSalon } = require("../src/validate");
-module.exports = (req,res)=>{res.setHeader("Content-Type","application/json; charset=utf-8");res.status(200).send(JSON.stringify(loadSalon()));};
--- salon_project/src/email.js
+++
@@ -1,4 +0,0 @@
-const nodemailer=require("nodemailer");
-function makeTransport(env){return nodemailer.createTransport({host:env.SMTP_HOST,port:Number(env.SMTP_PORT||587),secure:String(env.SMTP_SECURE||"false")==="true",auth:{user:env.SMTP_USER,pass:env.SMTP_PASS}});}
-async function sendAppointmentMail(t,env,a){await t.sendMail({from:env.FROM_ADDRESS||env.SMTP_USER,to:env.TEAM_INBOX,subject:`Terminanfrage ${a.name}`,html:`<p>${a.name}<br>${a.service}<br>${a.datetime}<br>${a.contact}</p>`});}
-module.exports={makeTransport,sendAppointmentMail};
